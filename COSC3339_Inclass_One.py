290af7ebd952e3ecdf73c8a1f08243358682cafc
DeshawnKing <157555624+DeshawnKing@users.noreply.github.com>
2026-01-15 14:59:52 -0600

    Update COSC3339_Inclass_One.py
    In the second branch change the return, and two other changes in the last 10 lines of calculate_shipping_cost.

--- COSC3339_Inclass_One.py
+++ COSC3339_Inclass_One.py
@@ -43,16 +43,16 @@ def calculate_shipping_cost(weight, destination):
         if weight <= 5:
             cost = base_cost
         else:
-            # Over 5 lbs, add $5 per extra lb
-            extra_weight = weight - 5
-            cost = base_cost + (extra_weight * 5.0)
+            # Over 3 lbs, add $10 per extra lb
+            extra_weight = weight - 3
+            cost = base_cost + (extra_weight * 10.0)
             
     else:
         # Unknown destination
         print(f"Error: Unknown destination {destination}")
         return None
 
-    return cost
+    return weight
 
 
 # This method uses funky logic. Rewrite it using different loop structures
